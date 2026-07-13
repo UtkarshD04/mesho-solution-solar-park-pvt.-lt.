5633131ea0f38881e93574da91753ad1a69277ed
Utkarsh Dwivedi <dutkarsh666@gmail.com>
2026-07-13 16:13:33 +0530

    Change mail

--- Frontend/src/components/layout/Footer.jsx
+++ Frontend/src/components/layout/Footer.jsx
@@ -133,7 +133,7 @@ export default function Footer() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
                 </div>
-                <a href="mailto:aseemmishra@gmail.com" className="hover:text-[#20b2aa] transition-colors">aseemmishra@gmail.com</a>
+                <a href="mailto:aseemmishra@mmyzo.com" className="hover:text-[#20b2aa] transition-colors">aseemmishra@mmyzo.com</a>
               </li>
 
               {/* Working Hours */}
